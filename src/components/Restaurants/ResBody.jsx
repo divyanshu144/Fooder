e8bc1083a6580e2c64256b8221a562f7d1aacb07
divyanshu144 <divyanshucharak14@gmail.com>
2024-09-12 02:17:31 +0530

    asjusted css

--- src/components/Restaurants/ResBody.jsx
+++ src/components/Restaurants/ResBody.jsx
@@ -49,7 +49,7 @@ function ResBody() {
         <Shimmer/> 
       ) : (
         <div className="body">
-          <div className="filter flex m-4 p-4 ml-16 flex items-center space-x-12 bg-white  rounded-lg">
+          <div className="filter flex m-4 p-4 ml-80 flex items-center space-x-12 bg-white rounded-lg">
           <div className="search space-x-4 ">
               <input
                   type="text"
